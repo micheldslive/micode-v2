e9821791dc95a8e75f3cd8cae8a8e7f561930c88
Michel Domingos <micheldslive@gmail.com>
2022-11-05 13:45:07 -0300

    chore: adding new projects to the portfolio

--- src/services/projects.ts
+++ src/services/projects.ts
@@ -1,3 +1,10 @@
+import Pomodoro from '@/assets/projects/pomodoro.jpg'
+import Movies from '@/assets/projects/movies.jpg'
+import Weather from '@/assets/projects/weather.jpg'
+import Calendar from '@/assets/projects/calendar.jpg'
+import TodoList from '@/assets/projects/todolist.jpg'
+import Healthy from '@/assets/projects/healthy.jpg'
+
 import Picinguaba from '@/assets/projects/picinguaba.jpg'
 import Nubank from '@/assets/projects/nubank.jpg'
 import Netflix from '@/assets/projects/netflix.jpg'
@@ -10,55 +17,97 @@ import Icone from '@/assets/projects/icone-api.jpg'
 export const projects = [
   {
     id: 1,
+    name: 'Pomodoro APP',
+    url: Pomodoro,
+    text: 'A pomodoro app using front end mentor layout.',
+    href: 'https://pomodoro-app-micode.vercel.app/',
+  },
+  {
+    id: 2,
+    name: 'Movies Challenge',
+    url: Movies,
+    text: 'Projeto realizado para teste de Front-End da Empresa DOT.',
+    href: 'https://movies-challenge-dot.vercel.app/',
+  },
+  {
+    id: 3,
+    name: 'Weather Challenge',
+    url: Weather,
+    text: 'Projeto realizado para teste de Front-End da Empresa TDS.',
+    href: 'https://weather-tds.netlify.app/',
+  },
+  {
+    id: 4,
+    name: 'Mi Calendar',
+    url: Calendar,
+    text: 'Projeto de estudo de caso do nextjs + react-scheduler.',
+    href: 'https://mi-calendar.vercel.app/',
+  },
+  {
+    id: 5,
+    name: 'TodoList Challenge',
+    url: TodoList,
+    text: 'Projeto realizado para teste de Front-End da Empresa EM.',
+    href: 'https://todo-list-em.vercel.app/',
+  },
+  {
+    id: 6,
+    name: 'Healthy Food',
+    url: Healthy,
+    text: 'Projeto realizado para teste de Front-End da Empresa GCB',
+    href: 'https://healthy-food-micode.vercel.app/',
+  },
+  {
+    id: 7,
     name: 'Vila Picinguaba',
     url: Picinguaba,
     text: 'Projeto final do Gama Experience #39.',
     href: 'https://vilapicinguaba.vercel.app/',
   },
   {
-    id: 2,
+    id: 8,
     name: 'Venom',
     url: Venom,
     text: 'Venom é um pequeno projeto para treinar minhas skills no ReactJS.',
     href: 'https://venom-carnage.vercel.app/',
   },
   {
-    id: 3,
+    id: 9,
     name: 'Netflix',
     url: Netflix,
     text: '"Projeto com objetivo de recriar a seleção de perfil e a home page da Netflix.',
     href: 'https://minhanetflix.vercel.app/',
   },
   {
-    id: 4,
+    id: 10,
     name: 'Nubank',
     url: Nubank,
     text: '"Projeto com objetivo de recriar a home do Nubank utilizando minhas skills.',
     href: 'https://nu-bank.vercel.app/',
   },
   {
-    id: 5,
+    id: 11,
     name: 'Gama Learning',
     url: Gamalearning,
     text: 'Gama Academy (Learning) é um pequeno projeto E-learning.',
     href: 'https://gamalearning.vercel.app/',
   },
   {
-    id: 6,
+    id: 12,
     name: 'Casca Grossa',
     url: CascaGrossa,
     text: 'Casca Grossa é um pequeno projeto de e-commerce.',
     href: 'https://casca-grossa.vercel.app/',
   },
   {
-    id: 7,
+    id: 13,
     name: 'Workpoint',
     url: Workpoint,
     text: 'Meu primeiro projeto feito com ReactJS, Case de estudo.',
     href: 'https://workpoint.netlify.app/',
   },
   {
-    id: 8,
+    id: 14,
     name: 'Icone Digital',
     url: Icone,
     text: 'Api do teste prático para feira da Gama Academy.',
